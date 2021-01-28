dad5b157dd13661807766c5f661b65ebe68d545c
zzzwenxing <1714379174@qq.com>
2021-01-28 17:23:04 +0800

    线程

---
+++ src/com/zzzwx/MultiThread/DeadLockDemo.java
@@ -0,0 +1,62 @@
+package com.zzzwx.MultiThread;
+
+/**
+ * @author zhuwenxing
+ * @ClassName DeadLockDemo
+ * @date 2021-01-28 17:12
+ * @description: 线程 A 持有资源 2，线程 B 持有资源 1，他们同时都想申请对方的资源
+ * 所以这两个线程就会互相等待而进入死锁状态。
+ * @version: 1.0
+ */
+public class DeadLockDemo {
+    private static Object resource1 = new Object();//资源 1
+    private static Object resource2 = new Object();//资源 2
+
+    public static void main(String[] args) {
+        new Thread(() -> {
+            synchronized (resource1) {
+                System.out.println(Thread.currentThread() + "get resource1");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(Thread.currentThread() + "waiting get resource2");
+                synchronized (resource2) {
+                    System.out.println(Thread.currentThread() + "get resource2");
+                }
+            }
+        }, "线程 1").start();
+
+        /*new Thread(() -> {
+            synchronized (resource2) {
+                System.out.println(Thread.currentThread() + "get resource2");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(Thread.currentThread() + "waiting get resource1");
+                synchronized (resource1) {
+                    System.out.println(Thread.currentThread() + "get resource1");
+                }
+            }
+        }, "线程 2").start();*/
+
+        // 破坏死锁，让线程1,2都可执行
+        new Thread(() -> {
+            synchronized (resource1) {
+                System.out.println(Thread.currentThread() + "get resource1");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(Thread.currentThread() + "waiting get resource2");
+                synchronized (resource2) {
+                    System.out.println(Thread.currentThread() + "get resource2");
+                }
+            }
+        }, "线程 2").start();
+    }
+}
