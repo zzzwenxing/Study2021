eccf6d97e7a67a43158ed9ee1f4e1f585e0e7fae
zzzwenxing <1714379174@qq.com>
2021-01-28 09:51:32 +0800

    Initial commit

---
+++ src/hello.java
@@ -0,0 +1,12 @@
+/**
+ * @author zhuwenxing
+ * @ClassName hello
+ * @date 2021-01-28 09:49
+ * @description: hello
+ * @version: 1.0
+ */
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("hello,2021");
+    }
+}
