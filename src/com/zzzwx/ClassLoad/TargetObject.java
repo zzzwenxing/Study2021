11a29a16f7d58e8f432791b454e0aeb2a4408fab
zzzwenxing <1714379174@qq.com>
2021-01-29 14:49:21 +0800

    ClassLoad 反射

--- src/com/zzzwx/ClassLoad/TargetObject.java
+++ src/com/zzzwx/ClassLoad/TargetObject.java
@@ -4,7 +4,7 @@ package com.zzzwx.ClassLoad;
  * @author zhuwenxing
  * @ClassName TargetObject
  * @date 2021-01-29 13:53
- * @description: TargetObject
+ * @description: 反射类
  * @version: 1.0
  */
 public class TargetObject {
